78b1f50ff752ee815fc180808a8a235b9842bc5d
danilodeLuca <danilo.pereiradeluca@gmail.com>
2017-11-06 13:48:09 -0200

    fixing spaces to tabs

--- src/test/java/xyz/luan/facade/HttpFacadeTest.java
+++ src/test/java/xyz/luan/facade/HttpFacadeTest.java
@@ -8,19 +8,19 @@ import org.junit.Test;
 
 public class HttpFacadeTest {
 
-    @Test
+	@Test
 	public void userTestNoProtocol() throws MalformedURLException {
-        HttpFacade facade = new HttpFacade("luan.xyz");
-        facade.user("luan", "mypass123");
+		HttpFacade facade = new HttpFacade("luan.xyz");
+		facade.user("luan", "mypass123");
 		assertEquals("http://luan:mypass123@luan.xyz", facade.getUrl());
-        assertEquals("Basic bHVhbjpteXBhc3MxMjM=", facade.header("Authentication"));
-    }
+		assertEquals("Basic bHVhbjpteXBhc3MxMjM=", facade.header("Authentication"));
+	}
 
-    @Test
+	@Test
 	public void userTestWithProtocol() throws MalformedURLException {
-        HttpFacade facade = new HttpFacade("https://luan.xyz");
-        facade.user("luan", "mypass123");
-        assertEquals("https://luan:mypass123@luan.xyz", facade.getUrl());
-        assertEquals("Basic bHVhbjpteXBhc3MxMjM=", facade.header("Authentication"));
-    }
+		HttpFacade facade = new HttpFacade("https://luan.xyz");
+		facade.user("luan", "mypass123");
+		assertEquals("https://luan:mypass123@luan.xyz", facade.getUrl());
+		assertEquals("Basic bHVhbjpteXBhc3MxMjM=", facade.header("Authentication"));
+	}
 }
